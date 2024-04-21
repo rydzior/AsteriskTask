aad363fbd0535d445346580b7adf1dae8e0b093b
Rydziu <kamilrydarowicz@gmail.com>
2024-04-21 14:57:18 +0200

    Test Update

--- src/test/java/com/ocado/AsteriskTest.java
+++ src/test/java/com/ocado/AsteriskTest.java
@@ -1,5 +1,6 @@
 package com.ocado;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -22,4 +23,52 @@ class AsteriskTest {
         String fileContents = new String(Files.readAllBytes(Paths.get(fileName)));
         assertTrue(Asterisk.verifyIAMRolePolicy(fileContents));
     }
+
+    @Test
+    void IsEmpty() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData.json";
+        String fileContents = new String(Files.readAllBytes(Paths.get(fileName)));
+        assertFalse(fileContents.isEmpty());
+    }
+
+    @Test
+    void fileExists() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData2.json";
+        assertTrue(Files.exists(Paths.get(fileName)));
+    }
+
+    @Test
+    void fileExists2() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData2.json";
+        assertFalse(Files.notExists(Paths.get(fileName)));
+    }
+
+    @Test
+    void DirectoryFile() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData2.json";
+        assertFalse(Files.isDirectory(Paths.get(fileName)));
+    }
+
+    @Test
+    void IsBlank() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData.json";
+        String fileContents = new String(Files.readAllBytes(Paths.get(fileName)));
+        assertFalse(fileContents.isBlank());
+    }
+
+    @Test
+    void RegularFile() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData2.json";
+        assertTrue(Files.isRegularFile(Paths.get(fileName)));
+    }
+
+    @Test
+    void LinkFile() throws IOException {
+        String fileName = "src/test/java/com/ocado/asteriskData.json";
+        assertFalse(Files.isSymbolicLink(Paths.get(fileName)));
+    }
+
+
+
+
 }
